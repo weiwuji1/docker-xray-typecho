859925ab22a4be0f69297b1e9a5d721199b52568
weiwuji1 <weibubu@gmail.com>
2023-04-08 13:55:14 +0800

    Update install-1.sh

--- install-1.sh
+++ install-1.sh
@@ -253,23 +253,23 @@ read -p "Enter your Cloudflare Account ID: " CF_ID
 read -p "Enter your Cloudflare Zone ID: " ZONE_ID
 read -p "Enter your Cloudflare Token: " CF_TOKEN
 
-sed -i "s/DB_USER/$DB_USER/g" docker-compose.yml
-sed -i "s/DB_PASS/$DB_PASS/g" docker-compose.yml
-sed -i "s/DB_NAME/$DB_NAME/g" docker-compose.yml
-sed -i "s/cf_account_id/$CF_ID/g" docker-compose.yml
-sed -i "s/cf_zonet_id/$ZONE_ID/g" docker-compose.yml
-sed -i "s/cf_token/$CF_TOKEN/g" docker-compose.yml
-sed -i "s/yourdomain.com/$DOMAIN/g" docker-compose-cf.yml
+sed -i "s/DB_USER/$DB_USER/g" ./typecho/docker-compose.yml
+sed -i "s/DB_PASS/$DB_PASS/g" ./typecho/docker-compose.yml
+sed -i "s/DB_NAME/$DB_NAME/g" ./typecho/docker-compose.yml
+sed -i "s/cf_account_id/$CF_ID/g" ./typecho/docker-compose.yml
+sed -i "s/cf_zonet_id/$ZONE_ID/g" ./typecho/docker-compose.yml
+sed -i "s/cf_token/$CF_TOKEN/g" ./typecho/docker-compose.yml
+sed -i "s/yourdomain.com/$DOMAIN/g" ./typecho/docker-compose-cf.yml
 
 # Modify domain name in nginx config
-sed -i "s/yourdomain.com/$DOMAIN/g" nginx/conf.d/default.conf
+sed -i "s/yourdomain.com/$DOMAIN/g" ./typecho/nginx/conf.d/default.conf
 
 # Modify UUID and email in Xray config
 read -p "Enter Xray UUID: " XRAY_UUID
 read -p "Enter email for Xray: " XRAY_EMAIL
 
-sed -i "s/Your-U-U-ID-HERE/$XRAY_UUID/g" xray/config/config.json
-sed -i "s/admin@yourdomain.com/$XRAY_EMAIL/g" xray/config/config.json
+sed -i "s/Your-U-U-ID-HERE/$XRAY_UUID/g" ./typecho/xray/config/config.json
+sed -i "s/admin@yourdomain.com/$XRAY_EMAIL/g" ./typecho/xray/config/config.json
 
 # Create and start containers
 sudo docker compose up -d
