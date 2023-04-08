f0ad16f608439491fb8fbca3a8598b2b9d532691
weiwuji1 <weibubu@gmail.com>
2023-04-08 12:51:04 +0800

    Update install-1.sh

--- install-1.sh
+++ install-1.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 # Install docker-ce and docker-compose
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install -y docker-ce docker-ce-cli containerd.io
-sudo systemctl start docker
-sudo systemctl enable docker
+#sudo yum install -y yum-utils
+#sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+#sudo yum install -y docker-ce docker-ce-cli containerd.io
+#sudo systemctl start docker
+#sudo systemctl enable docker
 
-sudo yum install -y epel-release
-sudo yum install -y python3-pip
-sudo pip3 install docker-compose
+#sudo yum install -y epel-release
+#sudo yum install -y python3-pip
+#sudo pip3 install docker-compose
+
+sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && sudo add-apt-repository "deb [arch=amd64] http://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable" && sudo apt-get update && sudo apt-get install -y docker-ce
+sudo apt-get install docker-compose-plugin
 
 # Creating Xray profiles
 mkdir -p /typecho/xray/config
